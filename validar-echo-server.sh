5e9b30642cffacc5bbf2274b07cd85b132d1bd04
Daniel Lovera <daanilo2497@gmail.com>
2024-09-03 02:13:43 -0300

    fix: update comparison of strings

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,7 +8,7 @@ server_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{
 server_port=12345
 
 container_response=$(docker run -q --network=$container_network $image_name sh -c "echo -n $signal | nc $server_ip $server_port 2>/dev/null")
-if [[ "$container_response" == "$signal" ]]; then
+if [ "$container_response" = "$signal" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
