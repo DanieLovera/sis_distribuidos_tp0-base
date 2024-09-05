e482720fcecf0aec3b07f73da20f0709d287a232
Daniel Lovera <daanilo2497@gmail.com>
2024-09-04 22:54:27 -0300

    refactor: simplify docker compose script

--- generar-compose.sh
+++ generar-compose.sh
@@ -4,9 +4,4 @@ nclients=$2
 echo "Docker Compose file name: '${file_name}'"
 echo "Number of clients: '${nclients}'"
 
-poetry run -C scripts python scripts/scale_clients.py ${file_name} ${nclients}
-
-exit_code=$?
-if [ $exit_code -eq 0 ]; then
-    echo "Docker Compose file generated with ${nclients} clients"
-fi
+python3 scale_clients.py ${file_name} ${nclients}
